e3a76a5cb35354763342bde9bc292e0bb7cf474b
guoguozhenhaowan <877801999@qq.com>
2020-01-07 12:31:42 +0800

    add function for onlyafter job

--- make_done_status.py
+++ make_done_status.py
@@ -5,7 +5,7 @@ class Node:
     def __init__(self,string):
         self.head = []
         self.tail = []
-        self.string = string 
+        self.string = string
 
     def add_tail(self, other):
         self.tail.append(other)
@@ -14,10 +14,10 @@ class Node:
     def add_head(self,other):
         self.head.append(other)
         other.tail.append(self)
-    
+
     def result(self):
         return self.string
-    
+
     def __repr__(self):
         return self.string
 
@@ -30,7 +30,16 @@ def beforelist_(Node):
         for sub in Node.head:
             list1.append(sub.string)
             list1.extend(beforelist_(sub))
-    return list1        
+    return list1
+
+def afterlist_(Node):
+    list1 = []
+    if Node.tail:
+        for sub in Node.tail:
+            list1.append(sub.string)
+            list1.extend(afterlist_(sub))
+    return list1
+
 
 def parser_job(jobfile,NodePattern):
     stringlist = []
@@ -39,21 +48,21 @@ def parser_job(jobfile,NodePattern):
     for line in open(jobfile):
         if line.strip().startswith("order"):
             _,a,after,b = line.strip().split()
-            
+
             if not a in stringlist:
                 stringlist.append(a)
                 TotalNodelist.append(Node(a))
             if not b in stringlist:
                 stringlist.append(b)
                 TotalNodelist.append(Node(b))
-                        
+
             if after == "after":
-                Node1 = TotalNodelist[stringlist.index(b)] 
+                Node1 = TotalNodelist[stringlist.index(b)]
                 Node2 = TotalNodelist[stringlist.index(a)]
             else:
-                Node1 = TotalNodelist[stringlist.index(a)] 
+                Node1 = TotalNodelist[stringlist.index(a)]
                 Node2 = TotalNodelist[stringlist.index(b)]
-            Node1.add_tail(Node2)    
+            Node1.add_tail(Node2)
             if NodePattern in Node1.string:
                 Nodelist.append(Node1)
             if NodePattern in Node2.string:
@@ -73,21 +82,71 @@ def parser_writejob(jobfile,namelist):
                 print(line.strip("\n"))
         else:
             print(line.strip("\n"))
-    
-def main(jobfile,jobpattern,ifNow=False):
+
+from itertools import groupby
+
+def groupby_job(line):
+    if line.startswith((" ","\t")):
+        return "job"
+    elif line.startswith("job"):
+        return "job_mark"
+    elif line.startswith("log"):
+        return "dir"
+    elif line.startswith("order"):
+        return "order"
+    else:
+        return "other"
+
+def parser_writeafterjob(jobfile,outlist):
+    for groupname,group in groupby(open(jobfile),key=groupby_job):
+        grouplist = list(group)
+        #print(groupname,grouplist)
+        if groupname in ["other","dir"]:
+            for line in grouplist:
+                print(line.strip("\n"))
+        elif groupname=="job":
+            name = grouplist[0].strip().split()[1]
+            if name in outlist:
+                print("job_begin")
+                for line in grouplist:
+                    if "status" == line.strip()[:6]:
+                        split1 = line.strip().split()
+                        line = line.strip("\n").replace(split1[1],"waiting")
+                    print(line.strip("\n"))
+                print("job_end")
+        elif groupname=="order":
+            for line in grouplist:
+                name1 = line.strip().split()[1]
+                name2 = line.strip().split()[3]
+                if name1 in outlist and name2 in outlist:
+                    print(line.strip())
+
+def main(jobfile,jobpattern,ifNow=False, OnlyAfter=False):
     Nodelist = parser_job(jobfile,jobpattern)
     nowlist = [Node.string for Node in Nodelist]
     beforelist = []
+    afterlist = []
     for subNode in Nodelist:
         list1 = beforelist_(subNode)
         beforelist.extend(list1)
-    if ifNow:    
-        outlist = set(beforelist) | set(nowlist)    
+    for subNode in Nodelist:
+        list1 = afterlist_(subNode)
+        afterlist.extend(list1)
+    if ifNow:
+        outlist = set(beforelist) | set(nowlist)
     else:
         outlist = set(beforelist)
-    parser_writejob(jobfile,outlist)
+    if OnlyAfter:
+        if ifNow:
+            outlist = set(afterlist) | set(nowlist)
+        else:
+            outlist = set(afterlist)
+    # __import__('pdb').set_trace()
+    if not OnlyAfter:
+        parser_writejob(jobfile,outlist)
+    else:
+        parser_writeafterjob(jobfile,outlist)
 
-from fire import Fire            
-if __name__ == "__main__":    
+from fire import Fire
+if __name__ == "__main__":
     Fire(main)
-    
